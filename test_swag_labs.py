a8b3546a85b5570f1e04754e3a16ec8e2506b241
Norbert Wieczorek <95376043+DukeDeWett@users.noreply.github.com>
2022-09-22 12:38:57 +0200

    Update test_swag_labs.py

--- test_swag_labs.py
+++ test_swag_labs.py
@@ -3,21 +3,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import pytest
+
 
 driver = webdriver.Chrome(service=Service(r"C:\Program Files (x86)/chromedriver.exe"))
 
 
-# For some reason this doesn't work, but without this "pytest" tests don't work, so I need to import it in some way.
-# To solve this I have put its functionality in first and last tests
-@pytest.fixture()
-def setup_module():
+def test_setup():
     driver.implicitly_wait(10)
     driver.maximize_window()
-    yield
-    driver.close()
-    driver.quit()
-    print("Test completed")
 
 
 # Firstable I have sent keys for profile that is destined to fail, so then I could try to log into standard account
@@ -80,5 +73,10 @@ def test_closure():
     driver.implicitly_wait(5)
     finish = driver.find_element(By.ID, "finish")
     finish.click()
-    time.sleep(2)
+    time.sleep(1)
+
+
+def test_teardown():
+    driver.close()
     driver.quit()
+    print("Test completed")
