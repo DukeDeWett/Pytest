534bee6e0ef8e1754834d6652582135dc387db32
Norbert Wieczorek <95376043+DukeDeWett@users.noreply.github.com>
2022-11-19 12:33:39 +0100

    Update header.py

--- testUQ/page_objects_UQ/header.py
+++ testUQ/page_objects_UQ/header.py
@@ -2,9 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class Header:
-    BUTTON1 = By.ID, "button1"
-    BUTTON2 = By.ID, 'button2'
-    BUTTON3 = By.ID, "idExample"
-    LINK1 = By.LINK_TEXT, "Click me using this link text!"
-    BUTTON4 = By.CLASS_NAME, "buttonClass"
-    BUTTON5 = By.NAME, "button1"
+    BUTTON_CLICK_ME = By.ID, "button1"
+    BUTTON_RAISE = By.ID, 'button2'
+    BUTTON_GREEN_ID = By.ID, "idExample"
+    LINK_ON_BLUE = By.LINK_TEXT, "Click me using this link text!"
+    BUTTON_CLASS_NAME_ON_BLUE = By.CLASS_NAME, "buttonClass"
+    BUTTON_NAME_ON_BLUE = By.NAME, "button1"
