d23b2ed1d87b0a5e48a283bc5e3ee85082febfb4
Norbert Wieczorek <95376043+DukeDeWett@users.noreply.github.com>
2022-12-06 21:36:02 +0100

    Update header.py

--- testUQ/page_objects_UQ/header.py
+++ testUQ/page_objects_UQ/header.py
@@ -1,7 +1,21 @@
 from selenium.webdriver.common.by import By
+from enum import Enum
 
 
-class Header:
+class BasePageObject(Enum):
+    @property
+    def by(self):
+        return self.value[1]
+
+    @property
+    def locator(self):
+        return self.value[0]
+
+    def find_element(self, driver):
+        return f"{self.locator} {self.by} {driver}"
+
+
+class Header(BasePageObject):
     BUTTON_CLICK_ME = By.ID, "button1"
     BUTTON_RAISE = By.ID, 'button2'
     BUTTON_GREEN_ID = By.ID, "idExample"
