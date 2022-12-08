c2e6c068614d5c8a82e5babb60754f63c11a2d2e
Norbert Wieczorek <95376043+DukeDeWett@users.noreply.github.com>
2022-12-08 17:23:17 +0100

    Update footer.py

--- testUQ/page_objects_UQ/footer.py
+++ testUQ/page_objects_UQ/footer.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
+from page_objects_UQ.base_page_object import BasePageObject
 
 
-class Footer:
+class Footer(BasePageObject):
     BUTTON_TOP_ON_GREY = By.XPATH, "//*[@id='button1']"
     BUTTON_SECOND_ON_GREY = By.XPATH, "(//button[@id='button1'])[2]"
     BUTTON_THIRD_ON_GREY = By.XPATH, "//*[@id='button1' and text()='Xpath Button 1']"
